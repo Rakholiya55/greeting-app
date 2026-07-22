7d9b790f424f3a9eed61d19b33b0c5c0f6258b41
Rakholiya55 <deeprakholiya55@gmail.com>
2026-07-21 22:38:40 -0400

    Convert app to a web server, add Kubernetes Deployment and Service manifests

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN npm install
 
 COPY . .
 
+EXPOSE 3000
+
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// app.js
+const http = require("http");
+
 function greet(name) {
   if (!name) {
     throw new Error("name cannot be empty");
@@ -6,6 +7,17 @@ function greet(name) {
   return `Hello, ${name}!`;
 }
 
-console.log(greet("World"));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(greet("World") + "\n");
+});
+
+const PORT = 3000;
+
+if (require.main === module) {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
 
 module.exports = { greet };
